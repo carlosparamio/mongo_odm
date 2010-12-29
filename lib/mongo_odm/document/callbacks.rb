cd59b4ef5a58bef2021fdea4e191d58b40e2f441
Steve Sloan <steve@finagle.org>
2010-12-28 23:52:00 -0800

    Callbacks: execute callbacks on documents in fields, including in array fields.

--- lib/mongo_odm/document/callbacks.rb
+++ lib/mongo_odm/document/callbacks.rb
@@ -11,19 +11,56 @@ module MongoODM
         extend ActiveModel::Callbacks
 
         alias_method_chain :initialize, :callbacks
+        alias_method_chain :run_callbacks, :fields
 
         define_model_callbacks :initialize, :only => :after
         define_model_callbacks :save, :create, :update, :destroy
       end
-      
+
       module InstanceMethods
         def initialize_with_callbacks(*args)
           initialize_without_callbacks(*args)
           run_callbacks(:initialize)
           self
         end
+
+        # execute callbacks on embedded documents, including arrays of documents.
+        def run_callbacks_with_fields( kind, *args, &blk )
+          run_callbacks_without_fields( kind, *args ) do
+            objs = self.class.fields.keys.map { |field|  self.send(field) }
+            objs.select! { |val|  val.respond_to?(:run_callbacks) || val.kind_of?(Array)  }
+            if objs.present?
+              Callbacks.run_multiple( objs, 0, kind, *args, &blk )
+            else
+              blk.call(self)
+            end
+          end
+        end
       end
 
+    protected
+      # execute callbacks on multiple objs, including arrays.
+      def self.run_multiple( objs, idx, kind, *args, &blk )
+        # find next suitable object, starting at idx
+        obj = nil
+        while idx < objs.size  do
+          obj = objs[idx]
+          break  if obj.respond_to?(:run_callbacks) || obj.kind_of?(Array)
+          idx += 1
+        end
+        return blk.call(self)  unless obj
+
+        # warning: recursion ahead
+        if obj.kind_of?(Array)
+          Callbacks.run_multiple( obj, 0, kind, *args ) do
+            Callbacks.run_multiple( objs, idx + 1, kind, *args, &blk )
+          end
+        else
+          obj.run_callbacks( kind, *args )  do |obj|
+            Callbacks.run_multiple( objs, idx + 1, kind, *args, &blk )
+          end
+        end
+      end
     end
   end
 end
--- spec/mongo_odm/document/callbacks_spec.rb
+++ spec/mongo_odm/document/callbacks_spec.rb
@@ -2,5 +2,103 @@
 require "spec_helper"
 
 describe MongoODM::Document::Callbacks do
-    
+
+  describe "with a document" do
+    it "initialize callback" do
+      doc = Class.new(BlankSlate)  do
+        attr_accessor :initialized
+        after_initialize { |obj|  obj.initialized = true  }
+      end.new
+      doc.initialized.should be_true
+    end
+
+    it "save callbacks" do
+      doc = Class.new(Shape)  do
+        before_save :on_before_save
+         after_save :on_after_save
+      end.new
+      doc.should_receive(:on_before_save).once.and_return(true)
+      doc.should_receive(:on_after_save).once.and_return(true)
+      doc.save
+    end
+
+    it "create callbacks" do
+      doc = Class.new(Shape)  do
+        before_create :on_before_create
+         after_create :on_after_create
+      end.new
+      doc.should_receive(:on_before_create).once.and_return(true)
+      doc.should_receive(:on_after_create).once.and_return(true)
+      doc.save
+    end
+
+    it "update callbacks" do
+      doc = Class.new(Shape)  do
+        before_update :on_before_update
+         after_update :on_after_update
+      end.new
+      doc.should_receive(:on_before_create).never
+      doc.should_receive(:on_after_create).never
+      doc.save
+
+      doc.color = 'cyan'
+      doc.should_receive(:on_before_update).once.and_return(true)
+      doc.should_receive(:on_after_update).once.and_return(true)
+      doc.save
+    end
+
+    it "destroy callbacks" do
+      doc = Class.new(Shape)  do
+        before_destroy :on_before_destroy
+         after_destroy :on_after_destroy
+      end.new
+      doc.should_receive(:on_before_create).never
+      doc.should_receive(:on_after_create).never
+      doc.save
+
+      doc.should_receive(:on_before_destroy).once.and_return(true)
+      doc.should_receive(:on_after_destroy).once.and_return(true)
+      doc.destroy
+    end
+  end
+
+  describe "with an embedded document" do
+    before do
+      doc_class = Class.new(Shape)  do
+        before_save :on_before_save
+         after_save :on_after_save
+      end
+      @parent = Class.new(BlankSlate)  do
+        field :thing, doc_class
+      end.new
+      @parent.thing = @doc = doc_class.new
+    end
+
+    it "save callbacks" do
+      @doc.should_receive(:on_before_save).once.and_return(true)
+      @doc.should_receive(:on_after_save).once.and_return(true)
+      @parent.save
+    end
+  end
+
+  describe "with an embedded array of documents" do
+    before do
+      doc_class = Class.new(Shape)  do
+        before_save :on_before_save
+         after_save :on_after_save
+      end
+      @parent = Class.new(BlankSlate)  do
+        field :things, Array
+      end.new
+      @parent.things = @docs = %w{red green blue}.map { |color|  doc_class.new( :color => color )  }
+    end
+
+    it "save callbacks" do
+      @docs.each  do |doc|
+        doc.should_receive(:on_before_save).once.and_return(true)
+        doc.should_receive(:on_after_save).once.and_return(true)
+      end
+      @parent.save
+    end
+  end
 end
