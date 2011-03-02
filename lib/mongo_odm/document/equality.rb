19711453b47a82d562e03f41ea1c7bae82a8f111
Nikita Baksalyar <n.baksalyar@yandex.ru>
2011-03-02 13:40:10 +0500

    BSON::DBRef comparison workaround.

--- lib/mongo_odm/document/equality.rb
+++ lib/mongo_odm/document/equality.rb
@@ -7,7 +7,17 @@ module MongoODM
 
       module InstanceMethods
         def eql?(other)
-          other.is_a?(self.class) && _id == other._id && attributes == other.attributes
+          # Comparing attributes manually since Ruby Mongo driver doesn't support DBRef objects comparison
+          return false if attributes.keys != other.attributes.keys
+          attributes.each_pair do |key, value|
+            if value.is_a?(BSON::DBRef) and other.attributes[key].is_a?(BSON::DBRef)
+              return false if value.to_hash != other.attributes[key].to_hash
+              next
+            end
+            return false if value != other.attributes[key]
+          end
+
+          other.is_a?(self.class) && _id == other._id
         end
         alias :== :eql?
         
