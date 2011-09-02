d23ee1ac77d8a56d7b6632d05521711d49b11916
Dusty Doris <dusty@doris.name>
2011-09-02 08:32:32 -0400

    only mark attribute dirty if it changes

--- lib/mongo_odm/document/attribute_methods/dirty.rb
+++ lib/mongo_odm/document/attribute_methods/dirty.rb
@@ -16,19 +16,21 @@ module MongoODM
           alias_method_chain :save, :dirty
           alias_method_chain :reload, :dirty
         end
-    
+
         module InstanceMethods
           def initialize_with_dirty(*args)
             initialize_without_dirty(*args)
             changed_attributes.clear
             self
           end
-        
+
           def write_attribute_with_dirty(attr_name, value)
-            send("#{attr_name}_will_change!") if respond_to?("#{attr_name}_will_change!")
+            if respond_to?("#{attr_name}_will_change!") && (read_attribute(attr_name) != value)
+              send("#{attr_name}_will_change!")
+            end
             write_attribute_without_dirty(attr_name, value)
           end
-      
+
           def save_with_dirty(*args)
             if status = save_without_dirty(*args)
               @previously_changed = changes
@@ -36,7 +38,7 @@ module MongoODM
             end
             status
           end
-      
+
           def reload_with_dirty(*args)
             reload_without_dirty(*args).tap do
               @previously_changed = nil
--- spec/mongo_odm/document/attribute_methods/dirty_spec.rb
+++ spec/mongo_odm/document/attribute_methods/dirty_spec.rb
@@ -2,5 +2,23 @@
 require "spec_helper"
 
 describe MongoODM::Document::AttributeMethods::Dirty do
-    
+
+  class TestDocument < Shape
+    validates_presence_of :color
+  end
+
+  it "marks as dirty when changed" do
+    doc = TestDocument.new :color => 'blue'
+    doc.color_changed?.should == false
+    doc.color = 'red'
+    doc.color_changed?.should == true
+  end
+
+  it "does not mark as dirty when the same" do
+    doc = TestDocument.new :color => 'blue'
+    doc.color_changed?.should == false
+    doc.color = 'blue'
+    doc.color_changed?.should == false
+  end
+
 end
